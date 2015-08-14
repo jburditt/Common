48dc42cba99887c2d96d4d40ca05a8824992329c
Jebb Burditt <jebb.burditt@gmail.com>
2015-08-14 12:41:26 -0700

    Create Settings.cs

---
+++ Settings.cs
@@ -0,0 +1,8 @@
+namespace Common
+{
+    public class Settings
+    {
+        public const string ShortDateFormat = "MMM dd, yyyy";
+        public const string InputDateFormat = "MM/dd/yy";
+    }
+}
