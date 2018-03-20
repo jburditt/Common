fff1a39d334a6eadd032d80192212a301ae4ffec
Jebb Burditt <jebb.burditt@gmail.com>
2018-03-20 15:21:57 -0700

    add to long and clone json

--- ObjectExtensions.cs
+++ ObjectExtensions.cs
@@ -1,9 +1,41 @@
-namespace common {
-  public class ObjectExtensions {
+namespace common 
+{
+  public class ObjectExtensions 
+  {
         public static int ToInt(this object source)
         {
             int n;
             int.TryParse(source+"", out n);
             return n;
+        }
+
+        public static long ToLong(this object source)
+        {
+            long n;
+            long.TryParse(source + "", out n);
+            return n;
+        }
+
+        /// <summary>
+        /// Perform a deep Copy of the object, using Json as a serialisation method. NOTE: Private members are not cloned using this method.
+        /// </summary>
+        /// <typeparam name="T">The type of object being copied.</typeparam>
+        /// <param name="source">The object instance to copy.</param>
+        /// <returns>The copied object.</returns>
+        public static T CloneJson<T>(this T source)
+        {
+            // Don't serialize a null object, simply return the default for that object
+            if (Object.ReferenceEquals(source, null))
+            {
+                return default(T);
+            }
+
+            // initialize inner objects individually
+            // for example in default constructor some list property initialized with some values,
+            // but in 'source' these items are cleaned -
+            // without ObjectCreationHandling.Replace default constructor values will be added to result
+            var deserializeSettings = new JsonSerializerSettings { ObjectCreationHandling = ObjectCreationHandling.Replace };
+
+            return JsonConvert.DeserializeObject<T>(JsonConvert.SerializeObject(source), deserializeSettings);
         }
 }
