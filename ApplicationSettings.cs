d7206b947f319ed28087a9d006a727ca26d9efe8
Jebb Burditt <jebb.burditt@gmail.com>
2020-08-24 16:50:46 -0700

    Update ApplicationSettings.cs

--- ApplicationSettings.cs
+++ ApplicationSettings.cs
@@ -1,16 +1,15 @@
 using Microsoft.Extensions.Configuration;
 using System.IO;
+using System.Reflection;
 
 // NOTE: Add nuget for Microsoft.Extensions.Configuration.Json and Microsoft.Extensions.Configuration.Binder
 namespace Common
 {
-    public class ApplicationSettings
+    public class AppSettings
     {
         public static string Setting1 { get; private set; }
         
-        private static IConfiguration Configuration;
-
-        static ApplicationSettings()
+        static AppSettings()
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
@@ -25,4 +24,39 @@ namespace Common
             Setting1 = Configuration.GetValue<string>("Setting1");
         }
     }
+    
+    public class XamarinAppSettings
+    {
+        using System.Linq
+        using System.Reflection;
+        
+        public static string Setting1 { get; private set; }
+        
+        static XamarinAppSettings()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonStream(GetEmbeddedResource("appSettings.json"))
+#if RELEASE
+                .AddJsonStream(GetEmbeddedResource("appSettings.Release.json"), true)
+#endif
+                ;
+
+
+            Configuration = builder.Build();
+
+            Setting1 = Configuration.GetValue<string>("Setting1");
+        }
+
+        static Stream GetEmbeddedResource(string resourceName)
+        {
+            // This line works on iOS and Android but NOT WPF
+            var assembly = assembly.GetCallingAssembly();
+
+            var resourcePath = assembly.GetManifestResourceNames()
+                .Single(str => str.EndsWith(resourceName));
+
+            return assembly.GetManifestResourceStream(resourcePath);
+        }
+    }
+}
 }
